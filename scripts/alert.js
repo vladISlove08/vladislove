a959dc344bde4f68baa8a65c8e213c47e5adaa97
Vladyslav <artemenkovladislav92@gmail.com>
2024-02-06 19:38:19 +0100

    0.0.7v commit
    fixed bugs, added different button pc/phone with output.

--- scripts/alert.js
+++ scripts/alert.js
@@ -3,12 +3,10 @@ function showAlert() {
     
 
     if (userName == null || userName == '') {
-        alert("You didn't write your username")
-        userName = prompt("Write your username");
-        if(userName == null || userName == '') {
+        
             alert("You're no authorized");
             return;
-        };
+        
         
 
         
@@ -22,6 +20,51 @@ function showAlert() {
 
 // Регистрируем обработчик события на событие 'load' объекта window
 window.onload = showAlert();
+
+function getText() {
+    var userInput = document.getElementById('mobileInput').value;
+    
+    if (userInput.trim() !== '') {
+        var outputElement = document.getElementById('output');
+        outputElement.textContent = `Your username:  ${userInput}`;
+    }
+}
+
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+};
+
+function toggleInputVisibility() {
+    let mobileInput = document.getElementById('mobileInput');
+    let mobileButton = document.getElementById('mobileButton');
+    
+    if (isMobileDevice()) {
+        mobileInput.style.display = 'block';
+        mobileButton.style.display = 'block'; // Отображаем поле ввода на мобильных устройствах
+    } else {
+        mobileInput.style.display = 'none'; 
+        mobileButton.style.display = 'none';// Скрываем поле ввода на других устройствах
+    }
+};
+
+function toggleInputVisibilitypc() {
+    let computerButton = document.getElementById('computerButton');
+    
+    
+    if (isMobileDevice()) {
+     
+        computerButton.style.display = 'none';// Скрываем поле ввода на других устройствах
+    } else {
+        
+        computerButton.style.display = 'block';// Скрываем поле ввода на других устройствах
+    }
+};
+
+toggleInputVisibility();
+toggleInputVisibilitypc();
+
+
+
 
 
 
