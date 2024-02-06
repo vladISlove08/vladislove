229b09296c3b23b4d7fdaa9359819d688684aa53
Vladyslav <artemenkovladislav92@gmail.com>
2024-02-06 19:47:13 +0100

    0.0.8v commit
    deleted no-needed mobile buttons, fixed prompt for mobile

--- scripts/alert.js
+++ scripts/alert.js
@@ -1,16 +1,10 @@
 function showAlert() {
     let userName = prompt("What's your username?");
-    
 
     if (userName == null || userName == '') {
         
             alert("You're no authorized");
             return;
-        
-        
-
-        
-
     } 
 
     let outputElement = document.getElementById('output');
@@ -19,7 +13,10 @@ function showAlert() {
 }
 
 // Регистрируем обработчик события на событие 'load' объекта window
-window.onload = showAlert();
+if (!isMobileDevice()) {
+    window.onload = showAlert();
+};
+
 
 function getText() {
     var userInput = document.getElementById('mobileInput').value;
@@ -34,34 +31,7 @@ function isMobileDevice() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 };
 
-function toggleInputVisibility() {
-    let mobileInput = document.getElementById('mobileInput');
-    let mobileButton = document.getElementById('mobileButton');
-    
-    if (isMobileDevice()) {
-        mobileInput.style.display = 'block';
-        mobileButton.style.display = 'block'; // Отображаем поле ввода на мобильных устройствах
-    } else {
-        mobileInput.style.display = 'none'; 
-        mobileButton.style.display = 'none';// Скрываем поле ввода на других устройствах
-    }
-};
 
-function toggleInputVisibilitypc() {
-    let computerButton = document.getElementById('computerButton');
-    
-    
-    if (isMobileDevice()) {
-     
-        computerButton.style.display = 'none';// Скрываем поле ввода на других устройствах
-    } else {
-        
-        computerButton.style.display = 'block';// Скрываем поле ввода на других устройствах
-    }
-};
-
-toggleInputVisibility();
-toggleInputVisibilitypc();
 
 
 
