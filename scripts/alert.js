d4259d5dab85121772461d6f06132dc169483704
Vladyslav <artemenkovladislav92@gmail.com>
2024-02-06 18:25:05 +0100

    v0.0.5
    added username prompt/alert + div for that

--- scripts/alert.js
+++ scripts/alert.js
@@ -1,3 +1,28 @@
-const BIRTHDAY = '18.04.1982'; // использовать заглавные буквы?
+function showAlert() {
+    let userName = prompt("What's your username?");
+    
 
-const AGE = someCode(BIRTHDAY); // а здесь?
+    if (userName == null || userName == '') {
+        alert("You didn't write your username")
+        userName = prompt("Write your username");
+        if(userName == null || userName == '') {
+            alert("You're no authorized");
+            return;
+        };
+        
+
+        
+
+    } 
+
+    let outputElement = document.getElementById('output');
+      outputElement.textContent = `Your username: ${userName}`
+
+}
+
+// Регистрируем обработчик события на событие 'load' объекта window
+window.onload = showAlert();
+
+
+
+
