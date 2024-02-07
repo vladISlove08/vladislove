4871f7ae607419f81c0d1c3e234455cf87ecbf24
Vladyslav <artemenkovladislav92@gmail.com>
2024-02-07 18:35:36 +0100

    0.0.9v microfix
    microfix with a string instead of alert about notAuthorized

--- scripts/alert.js
+++ scripts/alert.js
@@ -1,35 +1,48 @@
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+};
+
+
+
 function showAlert() {
     let userName = prompt("What's your username?");
-
     if (userName == null || userName == '') {
-        
-            alert("You're no authorized");
-            return;
+            let notAuthorized = `You're no authorized.`;
+            alert(notAuthorized);
+            let outputElement = document.getElementById('output');
+      outputElement.textContent = notAuthorized
+        return;
     } 
+    
+
 
     let outputElement = document.getElementById('output');
       outputElement.textContent = `Your username: ${userName}`
+      
 
 }
 
+
+
+
 // Регистрируем обработчик события на событие 'load' объекта window
+// онли для пк, на телефах отключено
 if (!isMobileDevice()) {
     window.onload = showAlert();
 };
 
 
 function getText() {
-    var userInput = document.getElementById('mobileInput').value;
+    let userInput = document.getElementById('mobileInput').value;
     
     if (userInput.trim() !== '') {
-        var outputElement = document.getElementById('output');
+        let outputElement = document.getElementById('output');
         outputElement.textContent = `Your username:  ${userInput}`;
     }
+    
 }
 
-function isMobileDevice() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-};
+
 
 
 
